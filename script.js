bcab94f993cf485a5b7c9c834fe651e9035551af
miyemicodes <lizzymiyemi@gmail.com>
2022-08-27 17:15:58 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use script';
+
+//navbar toggle
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function(){
+    this.classList.toggle("active");
+    header.classList.toggle("active");
+});
+
